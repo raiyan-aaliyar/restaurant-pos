7af3607d8ccaa23a3746af7dee4a50b55791c71b
raiyan-aaliyar <raiyanaaliyar@gmail.com>
2026-07-16 23:58:55 +0530

    env

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:yarpay/app.dart';
+import 'package:yarpay/data/services/supabase_service.dart';
 
-
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load();
+  await SupabaseService.initialize();
+
   runApp(
     const ProviderScope(
       child: RestoBillApp(),
